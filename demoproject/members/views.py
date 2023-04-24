40bb9fe9a73e26d8ddb7c648bdcdc7c703431a60
Dorjee <rishav345@gmail.com>
2023-04-24 13:45:51 -0400

    first commit

---
+++ demoproject/members/views.py
@@ -0,0 +1,38 @@
+
+# Create your views here.
+# from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+
+def index(request):
+ template = loader.get_template('myfirst.html')
+ return HttpResponse(template.render())
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  mydata = Member.objects.all().values()
+  template = loader.get_template('main.html')
+  context = {
+    'mymembers': mydata,
+  }
+  return HttpResponse(template.render())
+
